904b94982da0c92556e8445e6b1f7d82756066da
jkpubsrc <pubsrc@binary-overflow.de>
2020-03-13 06:40:29 +0100

    Order of mount point and device reverted

--- src/bin/mount.py
+++ src/bin/mount.py
@@ -24,11 +24,11 @@ for mi in mounter.getMountInfos2(isRegularDevice = True, isNetworkDevice=True):
 interesting.sort(key=lambda x: x.mountPoint)
 
 table = jk_console.SimpleTable()
-headRow = table.addRow("device", "mount point", "file system", "mode")
+headRow = table.addRow("mount point", "device", "file system", "mode")
 headRow.hlineAfterRow = True
 
 for mi in interesting:
-	table.addRow(mi.device, mi.mountPoint, mi.fsType, "r" if mi.isReadOnly else "rw")
+	table.addRow(mi.mountPoint, mi.device, mi.fsType, "r" if mi.isReadOnly else "rw")
 
 print()
 table.print()
--- src/jk_mounting/__init__.py
+++ src/jk_mounting/__init__.py
@@ -1,13 +1,12 @@
 ﻿
 
-__version__ = "0.2020.1.13"
+__version__ = "0.2020.3.13"
 
 
 
 from .MountInfo import MountInfo
 from .MountOptions import MountOptions
 from .Mounter import Mounter
-
 
 
 
